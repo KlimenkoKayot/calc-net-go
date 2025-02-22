79d361e5ad14a488325d4afba282acf59f3c9683
Klimenko George <george_klimenko6@mail.ru>
2025-02-23 00:23:38 +0300

    Task и Result
    Написаны структуры для результата и задачи

--- internal/shared/models/models.go
+++ internal/shared/models/models.go
@@ -1,1 +1,17 @@
 package models
+
+import "time"
+
+type Task struct {
+	Id             int           `json:"id"`
+	FirstArgument  float64       `json:"arg1"`
+	SecondArgument float64       `json:"arg2"`
+	Operation      rune          `json:"operation"`
+	OperationTime  time.Duration `json:"operation_time"`
+}
+
+type Result struct {
+	Id     int     `json:"id"`
+	Status string  `json:"status"`
+	Result float64 `json:"result"`
+}
